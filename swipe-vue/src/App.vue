3f66461eb8f6752c7774b08089be153761de4cab
Valerie Wang <valerielle.wang@gmail.com>
2021-07-13 17:32:55 -0400

    updates welcome banner

--- swipe-vue/src/App.vue
+++ swipe-vue/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <HelloWorld msg="Welcome to SwipeApp, a Vue.js + TypeScript application" />
 </template>
 
 <script lang="ts">
