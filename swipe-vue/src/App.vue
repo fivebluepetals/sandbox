081cb8891b01f218eb22a7d5e6d5d544e58b9f96
Valerie Wang <valerielle.wang@gmail.com>
2021-07-19 14:49:38 -0400

    redo changes to v0.0.1

--- swipe-vue/src/App.vue
+++ swipe-vue/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <HelloWorld msg="Welcome to Swipe, a Vue.js + TypeScript app!" />
 </template>
 
 <script lang="ts">
